ba25ab42740ba402f2a6615b9ecdb0076de775dd
h1-the-swan <jason.portenoy@gmail.com>
2017-01-15 15:07:50 -0800

    vis broadcast feature in development is hidden. activate it using URL query params

--- receive_messages_from_timeline.js
+++ receive_messages_from_timeline.js
@@ -2,6 +2,18 @@
 var citationVis = citationVis || {};
 
 $( document ).on( "initComplete", function() {
+	// http://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+	function getParameterByName(name, url) {
+		if (!url) url = window.location.href;
+		name = name.replace(/[\[\]]/g, "\\$&");
+		var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+			results = regex.exec(url);
+		if (!results) return null;
+		if (!results[2]) return '';
+		return decodeURIComponent(results[2].replace(/\+/g, " "));
+	}
+	if (getParameterByName('rcvmsg') === null) return;
+
 	var egoGraphVis = citationVis.egoGraphVis;
 
 	$(window).on('storage', message_receive);
@@ -17,13 +29,33 @@ $( document ).on( "initComplete", function() {
 
 		// act on the message
 		if (message.command == 'timelineVis:paperItem:mouseover') highlightLinkedPapers(message.data.pid);
+		if (message.command == 'timelineVis:paperItem:mouseout') linkedPapersMouseout(message.data.pid);
 	}
 
 	function highlightLinkedPapers(paper_id) {
+		var highlightedNodes = [];
+
 		d3.selectAll(".node").filter(function(d) {
-			return d.targetPaperIDs && d.targetPaperIDs.indexOf(paper_id) != 0;
+			// return d.targetPaperIDs && d.targetPaperIDs.indexOf(paper_id) != -1;
+			if (d.targetPaperIDs && d.targetPaperIDs.indexOf(paper_id) != -1) {
+				highlightedNodes.push(d);
+				return true;
+			}
 		})
-		.attr("r", 20);
+		.classed("linkedToTimeline", true);
+
+		d3.selectAll(".link.toEgo").filter(function(d) {
+			return highlightedNodes.indexOf(d.source) != -1;
+		})
+		.classed("linkedToTimeline", true);
+	}
+
+	function linkedPapersMouseout(paper_id) {
+		// d3.selectAll(".node").filter(function(d) {
+		// 	return d.targetPaperIDs && d.targetPaperIDs.indexOf(paper_id) != -1;
+		// })
+		// .classed("linkedToTimeline", false);
+		d3.selectAll(".linkedToTimeline").classed("linkedToTimeline", false);
 	}
 });
 
